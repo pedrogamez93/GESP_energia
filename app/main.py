23e782d9e89d38917f731001d3b65de15288a44d
pedrogamez93 <pedrogamez93@hotmail.com>
2025-08-13 12:49:51 -0400

    Eliminar entornos virtuales del repositorio

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from app.api.v1 import auth  
 
-app = FastAPI()
+app = FastAPI(title="GESP Energia API")
+
+# Prefijo de versión
+app.include_router(auth.router, prefix="/api/v1/auth", tags=["Auth"])
 
 @app.get("/", tags=["Health"])
 def root():
@@ -9,3 +13,4 @@ def root():
 @app.get("/api/v1", tags=["Health"])
 def api_v1_root():
     return {"status": "ok", "message": "API up"}
+
