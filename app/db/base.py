d8b466787dfdede3655e3338c7b10f1a8dd12552
ernesto4664 <ernesto4664@gmail.com>
2025-10-08 18:21:22 -0300

    unidadinmuebles

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,7 @@
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+from app.db.models.unidad import Unidad                # noqa: F401
+from app.db.models.unidad_inmueble import UnidadInmueble  # noqa: F401
+from app.db.models.division import Division            # noqa: F401
